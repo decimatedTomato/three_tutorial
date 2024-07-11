70c50dbf27a68982b53ea610b5a575a64452142e
decimatedTomato <greentomatopower@gmail.com>
2024-07-11 19:03:44 +0200

    Cube

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,39 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-renderer.clear();
+const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cube = new THREE.Mesh(boxGeometry, material);
+scene.add(cube);
+
+// const latheGeometry = new THREE.LatheGeometry(
+// 	[
+// 		new THREE.Vector2(0, 1),
+// 		new THREE.Vector2(0, 2),
+// 		new THREE.Vector2(1, -1),
+// 		new THREE.Vector2(1, -2),
+// 		new THREE.Vector2(1, -3),
+// 	],
+// 	10,
+// 	0, Math.PI * 2
+// );
+// const lathe = new THREE.Mesh(latheGeometry, material);
+// scene.add(lathe);
+
+camera.position.z = 5;
+
+// White directional light at half intensity shining from the top.
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(-1, -1, 1);
+scene.add(directionalLight);
+
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
+
+function animate() {
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
